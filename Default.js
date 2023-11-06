830d56d6f5b7e8ff03496da6fdb789568cd988a9
IA7308 <naziqnaz13@gmail.com>
2023-11-06 23:09:25 +0700

    Penambahan Script

--- Default.js
+++ Default.js
@@ -5,15 +5,16 @@ var statusLogin = localStorage.getItem("statusLogin");
 var statusAdmin = localStorage.getItem("statusAdmin");
 
 function login(){
+    formlogn.action = "Dashboard.html";
     if(username.value == "" || password.value == ""){
         window.alert("Masukkan Password/Username !!")
     }else if(username.value == "Iqnaz" && password.value == "aa"){
-        formlogn.action = "Dashboard_Login.html";
+        //formlogn.action = "Dashboard_Login.html";
         localStorage.setItem("statusLogin", "true");
         localStorage.setItem("statusAdmin", "false");
     }else if(username.value == "AlifAdmin" && password.value == "alif123"){
         window.alert("Anda Login Sebagai Admin")
-        formlogn.action = "Dashboard.html";
+        //formlogn.action = "Dashboard.html";
         localStorage.setItem("statusLogin", "true");
         localStorage.setItem("statusAdmin", "true");
     }else{
@@ -24,10 +25,20 @@ function login(){
 }
 
 function cekLogin(){
+    var LoginBar = document.getElementById("LoginBar");
+    var signUpBar = document.getElementById("SignUpBar");
+    var porfileBar = document.getElementById("ProfileBar");
+    var signOutBar = document.getElementById("SignOutBar");
+    var profBar = document.getElementById("ProfBar");
     if(statusLogin == "false"){
         window.alert("Harap Login untuk menggunakan fitur ini");
+        porfileBar.style.display = 'none';
+        signOutBar.style.display = 'none';
         return false;
     }else{
+        profBar.style.display = 'none';
+        LoginBar.style.display = 'none';
+        signUpBar.style.display = 'none';
         return true;
     }
 }
@@ -38,4 +49,9 @@ function cekAdmin(){
         DropdownMenu.style.display = 'none';
         return false;
     }
+}
+
+function LogOut(){
+    localStorage.setItem("statusLogin", "false");
+    localStorage.setItem("statusAdmin", "false");
 }
